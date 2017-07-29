2fe8cbc0644c19351e3318d8a9b97d5602764426
hjk7 <hjk7@sfu.ca>
2017-07-29 01:22:46 -0700

    (1)ui bug fixed. (2)random word service renamed to word service

--- randomWordDictionary/src/app/random/random.component.ts
+++ randomWordDictionary/src/app/random/random.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { RandomWordService } from '../random-word.service';
+import { WordService } from '../word.service';
 import { SettingServiceService } from '../setting-service.service';
 
 @Component({
   selector: 'app-random',
   templateUrl: './random.component.html',
   styleUrls: ['./random.component.css'],
-  providers: [RandomWordService]
+  providers: [WordService]
 })
 export class RandomComponent implements OnInit {
 
     myData: string;
 
-    constructor(private randomWordService: RandomWordService, private settingService: SettingServiceService) { }
+    constructor(private wordService: WordService, private settingService: SettingServiceService) { }
 
     ngOnInit() {
         //let wordType = this.settingComponent.getCurrentWordType() == "any" ? null : this.settingComponent.getCurrentWordType();
         //console.log(`currnet word type is '${wordType}'`);
-        //this.randomWordService.getRandomWord(wordType).subscribe( //subscribe to the observable return method
+        //this.wordService.getRandomWord(wordType).subscribe( //subscribe to the observable return method
         //    data => this.myData = JSON.stringify(data),
         //    error => alert(error),
         //    () => console.log("finished")
@@ -28,7 +28,7 @@ export class RandomComponent implements OnInit {
     onRandomClick() {
         let wordType = this.settingService.GetCurrentWordType() == "any" ? null : this.settingService.GetCurrentWordType();
         
-        this.randomWordService.GetRandomWord(wordType).subscribe( //subscribe to the observable return method
+        this.wordService.GetRandomWord(wordType).subscribe( //subscribe to the observable return method
             data => this.myData = JSON.stringify(data),
             error => alert(error),
             () => console.log("finished")
--- randomWordDictionary/src/app/word.service.ts
+++ randomWordDictionary/src/app/word.service.ts
@@ -3,11 +3,13 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class RandomWordService {
+export class WordService {
 
-    baseUrl:string = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+    baseUrl: string = `api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
 
-    constructor(private http:Http) { }
+    constructor(private http: Http) {
+        this.baseUrl = window.location.protocol == 'https:' ? `https://${this.baseUrl}` : `http://${this.baseUrl}`;
+    }
 
     public GetRandomWord(wordType: string) {
         let url = wordType ? this.baseUrl + `&includePartOfSpeech=${wordType}` : this.baseUrl;
@@ -15,6 +17,5 @@ export class RandomWordService {
         return this.http.get(url)
             .map((res: Response) => res.json()); //convert observable into json format
     }
-
 
 }
