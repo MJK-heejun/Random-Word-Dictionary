ca24d37c7479ba3ea9784be67982262601ae6892
hjk7 <hjk7@sfu.ca>
2017-07-28 23:43:19 -0700

    http to https

--- randomWordDictionary/src/app/random-word.service.ts
+++ randomWordDictionary/src/app/random-word.service.ts
@@ -5,14 +5,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class RandomWordService {
 
+    baseUrl:string = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+
     constructor(private http:Http) { }
 
     public GetRandomWord(wordType: string) {
-        let url = "";
-        if (wordType)
-            url = `http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=${wordType}&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
-        else
-            url = `http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+        let url = wordType ? this.baseUrl + `&includePartOfSpeech=${wordType}` : this.baseUrl;
         console.log(`http call to: ${url}`);
         return this.http.get(url)
             .map((res: Response) => res.json()); //convert observable into json format
