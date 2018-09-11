c8682d51b81c5102d1ddd21034c5c624d9a9c805
hjk7 <hjk7@sfu.ca>
2018-09-10 23:43:32 -0700

    quick api key update

--- randomWordDictionary/src/app/word.service.ts
+++ randomWordDictionary/src/app/word.service.ts
@@ -6,26 +6,27 @@ import 'rxjs/add/operator/map';
 export class WordService {
 
     baseUrl: string = `api.wordnik.com/v4/`;    
-
+    apiKey: string = `d7e00125cd33554cd1c1a509b7f0091f3558955bb416ee24b`;    
+    
     constructor(private http: Http) {
         this.baseUrl = window.location.protocol == 'https:' ? `https://${this.baseUrl}` : `http://${this.baseUrl}`;
     }
 
     public GetRandomWord(wordType: string) {
-        let randomApi = `${this.baseUrl}words.json/randomWord?hasDictionaryDef=true&hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+        let randomApi = `${this.baseUrl}words.json/randomWord?hasDictionaryDef=true&hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${this.apiKey}`;
         let url = wordType ? `${randomApi}&includePartOfSpeech=${wordType}` : randomApi;
         return this.http.get(url)
             .map((res: Response) => res.json()); //convert observable into json format
     }
 
     public GetDefinition(word: string) {        
-        let url = `${this.baseUrl}word.json/${word}/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+        let url = `${this.baseUrl}word.json/${word}/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key=${this.apiKey}`;
         return this.http.get(url)
             .map((res: Response) => res.json()); //convert observable into json format
     }
 
     public GetAudioUrl(word: string) {
-        let url = `${this.baseUrl}word.json/${word}/audio?useCanonical=true&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+        let url = `${this.baseUrl}word.json/${word}/audio?useCanonical=true&limit=1&api_key=${this.apiKey}`;
         return this.http.get(url)
             .map((res: Response) => res.json()); //convert observable into json format
     }
